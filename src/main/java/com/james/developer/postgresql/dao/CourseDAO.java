863c90e0e62c1a0cd313a3e2f6e728ff4516f860
JRG <47917431+JR-G@users.noreply.github.com>
2020-09-24 10:36:36 +0100

    Build: added DAO files

---
+++ src/main/java/com/james/developer/postgresql/dao/CourseDAO.java
@@ -0,0 +1,8 @@
+package com.james.developer.postgresql.dao;
+
+import com.james.developer.postgresql.entities.Course;
+import org.springframework.data.repository.CrudRepository;
+import java.util.UUID;
+
+public interface CourseDAO extends CrudRepository<Course, UUID> {
+}
