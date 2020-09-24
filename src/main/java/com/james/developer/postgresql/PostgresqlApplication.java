407a027f56b9dde90c0a73da8f1a4cbb0b57c570
JRG <47917431+JR-G@users.noreply.github.com>
2020-09-24 14:25:17 +0100

    Build: added oauth function

--- src/main/java/com/james/developer/postgresql/PostgresqlApplication.java
+++ src/main/java/com/james/developer/postgresql/PostgresqlApplication.java
@@ -2,6 +2,9 @@ package com.james.developer.postgresql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -10,6 +13,18 @@ public class PostgresqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PostgresqlApplication.class, args);
+	}
+
+	@Configuration
+	static class OktaOAuth2WebSecurityConfigureAdapter extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.authorizeRequests().anyRequest().authenticated()
+					.and()
+					.oauth2ResourceServer().jwt();
+		}
 	}
 
 }
